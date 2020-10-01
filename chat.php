152ecdb5d573f2e20f7357a4bbd550f55a04ed4a
FazleRabbiBindu <fazlerabbibindu@gmail.com>
2020-10-02 00:22:07 +0600

    upload 01

---
+++ chat.php
@@ -0,0 +1,31 @@
+<?php
+
+if(isset($_POST['chat_send']))
+{
+    $message = $_POST['message'];
+}
+else
+{
+    $message = "No message yet";
+}
+
+?>
+<html>
+<head>
+</head>
+<body>
+    
+<div class="chat-area">
+<div class="chat-display">
+<?php echo $message; ?>
+</div>
+<div class="chat-submission">
+<form action="" method="post">
+<input type="text" name="message" id="">
+<input type="submit" name="chat_send" value="Send">
+</form>
+</div>
+</div>
+
+</body>
+</html>
